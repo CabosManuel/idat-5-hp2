ef5c2d3846ca0b2455480085409cfdb555617173
CabosManuel <cabos.apolaya@gmail.com>
2020-07-26 03:09:12 -0500

    buscar_peliculas

---
+++ EC3/SolucionEC3ManuelCabos/ProyectoEC3ManuelCabos/Controllers/PeliculasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ProyectoEC3ManuelCabos.Models;
+using PagedList;
+
+namespace ProyectoEC3ManuelCabos.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        BDVIDEOS2019Entities db = new BDVIDEOS2019Entities();
+        public ActionResult buscar_peliculas(string cod_gen= "",int año=0,int nPag=1)
+        {
+            ViewBag.vbCodGen = cod_gen;
+            ViewBag.vbAño = año;
+            ViewBag.vbGeneros = new SelectList(db.Genero.ToList(), "cod_gen", "cod_gen");
+
+            int nFilas = 5;
+            var listaGeneros = db.sp_buscar_peliculas(cod_gen, año).ToList().ToPagedList(nPag, nFilas);
+
+            return View(listaGeneros);
+        }
+
+       
+    }
+}
