4ce3f516ff11894b4510d6a6f7be6ed115e48bc4
CabosManuel <cabos.apolaya@gmail.com>
2020-08-18 17:44:57 -0500

    Eliminación de scripts

---
+++ S09 - SP Personalizados2/Proced_Almacenado_Neptuno2019_MVC.sql
@@ -0,0 +1,67 @@
+use Neptuno2019
+go
+
+-- proced. almacenado que liste los pedidos en base a un 
+-- codigo de cliente, debiendo mostrar:
+-- idpedido, fechapedido, cant_items, importe acumulado del pedido
+create procedure pa_pedidos_cliente
+@idcliente char(5)
+as
+	select p.IdPedido, 
+	       p.FechaPedido,
+		   count(dp.IdProducto) as cant_items,
+		   sum(dp.PrecioUnidad*dp.Cantidad) as importe_pedido
+	from Pedidos p inner join Detalles_Pedidos dp
+		on p.IdPedido=dp.IdPedido
+	where p.idcliente=@idcliente
+		group by p.IdPedido, p.FechaPedido
+go
+
+execute pa_pedidos_cliente 'anatr'
+go
+
+create procedure pa_listar_clientes
+as
+	select IdCliente, NombreCompañia, Pais, Telefono
+	from Clientes
+go
+
+create procedure pa_insertar_cliente
+@idcliente char(5),@nombre varchar(40),
+@pais varchar(15),@telefono varchar(24)
+as
+	insert into Clientes
+		values(@idcliente,@nombre,'','','','','','',@pais,@telefono,'')
+go
+
+select * from Clientes
+go
+
+------------- 25/06/2020 -----------------------
+create procedure pa_actualizar_cliente
+@idcliente char(5),@nombre varchar(40),
+@pais varchar(15),@telefono varchar(24)
+as
+	update Clientes
+		set NombreCompañia=@nombre,
+			Pais=@pais,
+			Telefono=@telefono
+	where IdCliente=@idcliente
+go
+
+create procedure pa_eliminar_cliente
+@idcliente char(5)
+as
+	delete from Clientes
+		where IdCliente=@idcliente
+go
+
+
+
+execute pa_eliminar_cliente 'ALFKI'
+go
+
+
+
+
+
---
+++ S09 - SP Personalizados2/Solucion_MVC_Neptuno/AppMVC_Neptuno/Controllers/Neptuno2019Controller.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AppMVC_Neptuno.Models;
+
+namespace AppMVC_Neptuno.Controllers
+{
+    public class Neptuno2019Controller : Controller
+    {
+        // definimos una variable del ADO Net Entity Data Model
+        Neptuno2019Entities db = new Neptuno2019Entities();
+
+        // GET: Neptuno2019
+        public ActionResult Listar_Clientes_tabla()
+        {
+            return View(db.Clientes.ToList());
+        }
+        
+        public ActionResult Listar_Clientes_SP()
+        {
+            return View(db.pa_listar_clientes().ToList());
+        }
+
+        public ActionResult Listar_Pedidos_Por_Cliente(string codcli="")
+        {
+            // tabla
+            ViewBag.CLIENTES1 = 
+                new SelectList(db.Clientes.ToList(), "IdCliente","NombreCompañia");
+
+            // proced. almacenado
+            ViewBag.CLIENTES2 = 
+                new SelectList(db.pa_listar_clientes().ToList(), "IdCliente", "NombreCompañia");
+
+            return View(db.pa_pedidos_cliente(codcli).ToList());
+        }
+
+        // GET
+        public ActionResult Insertar_Cliente_SP()
+        {
+            // obteniendo la lista de paises sin repetir
+            // linq
+            List<string> l_paises1 = (from c in db.Clientes
+                                      select c.Pais).Distinct().ToList();
+
+            
+
+            // enviando la lista de paises
+            ViewBag.PAISES = new SelectList(l_paises1);
+
+            // usando una tabla como modelo
+            return View(new Clientes());
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Insertar_Cliente_SP(Clientes obj)
+        {
+            // llamar al proced almacenado
+            db.pa_insertar_cliente(obj.IdCliente, obj.NombreCompañia, obj.Pais, obj.Telefono);
+
+            // lambda
+            List<string> l_paises2 = db.Clientes.Select(c => c.Pais).Distinct().ToList();
+            
+            // enviando la lista de paises
+            ViewBag.PAISES = new SelectList(l_paises2);
+
+            ViewBag.MENSAJE = "Nuevo Cliente grabado correctamente";
+
+            return View(obj);
+        }
+
+        ///////// 25/06/2020 /////////////////////
+
+        public ActionResult Actualizar_Cliente_SP(string codcli)
+        {
+            Clientes obj = db.Clientes.Find(codcli);
+
+            List<string> l_paises = 
+                db.Clientes.Select(x => x.Pais).Distinct().ToList();
+
+            ViewBag.PAISES = new SelectList(l_paises);
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult Actualizar_Cliente_SP(Clientes cli)
+        {
+            // si no hubiera error, actualizamos y nos redireccionamos
+            // hacia el listado de clientes
+            if (ModelState.IsValid)
+            { 
+                // Actualizar el Cliente
+                db.pa_actualizar_cliente(cli.IdCliente, cli.NombreCompañia,
+                                         cli.Pais, cli.Telefono);
+
+                //
+                return RedirectToAction("Listar_Clientes_SP");
+            }
+
+            List<string> l_paises =
+                db.Clientes.Select(x => x.Pais).Distinct().ToList();
+
+            ViewBag.PAISES = new SelectList(l_paises);
+
+            return View(cli);
+
+        }
+
+        public ActionResult Eliminar_Cliente_SP(string codcli)
+        {
+            Clientes obj = db.Clientes.Find(codcli);
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar_Cliente_SP(string codcli, Clientes xyz)
+        {
+            Clientes obj = db.Clientes.Find(codcli);
+
+            try
+            {
+                db.pa_eliminar_cliente(codcli);
+
+                return RedirectToAction("Listar_Clientes_SP");
+            }
+            catch (Exception)
+            {
+                ViewBag.MENSAJE = "Error al Eliminar al Cliente: " + obj.NombreCompañia;
+                return View(obj);
+            }
+
+        }
+
+
+
+        public ActionResult ListarClientesPaginacion()
+        {
+            return View(db.Clientes.ToList());
+        }
+
+
+        public ActionResult ListarClientesPaginacionSP()
+        {
+            return View(db.pa_listar_clientes().ToList());
+        }
+
+        // version 2 para eliminar
+        /*
+        public ActionResult Eliminar_Cliente_SP(string codcli)
+        {
+            Clientes obj = db.Clientes.Find(codcli);
+
+            return View(obj);
+        }
+
+        [HttpPost, ActionName("Eliminar_Cliente_SP")]
+        public ActionResult Eliminar_Cliente_desde_SP(string codcli)
+        {
+
+            Clientes obj = db.Clientes.Find(codcli);
+
+            try
+            {
+                db.pa_eliminar_cliente(codcli);
+
+                return RedirectToAction("Listar_Clientes_SP");
+            }
+            catch (Exception)
+            {
+                ViewBag.MENSAJE = "Error al Eliminar al Cliente: "+obj.NombreCompañia;
+                return View(obj);
+            }
+            
+        }
+        */
+
+        // version de 3 para eliminar
+        /*
+        public ActionResult Eliminar_Cliente_SP(string codcli)
+        {
+            Clientes obj = db.Clientes.Find(codcli);
+
+            TempData["codigo"] = codcli;
+
+            return View(obj);
+        }
+
+        [HttpPost] 
+        public ActionResult Eliminar_Cliente_desde_SP()
+        {
+            string codcli = TempData["codigo"].ToString();
+
+            Clientes obj = db.Clientes.Find(codcli);
+
+            try
+            {
+                db.pa_eliminar_cliente(codcli);
+
+                return RedirectToAction("Listar_Clientes_SP");
+            }
+            catch (Exception)
+            {
+                ViewBag.MENSAJE = "Error al Eliminar al Cliente: " + obj.NombreCompañia;
+                return View(obj);
+            }
+
+        }
+        */
+    }
+}
