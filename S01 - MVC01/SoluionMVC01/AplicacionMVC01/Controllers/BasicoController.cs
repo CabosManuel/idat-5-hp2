585403c5ccf718131e001e03aff63123ac3808ca
CabosManuel <cabos.apolaya@gmail.com>
2020-08-18 15:26:35 -0500

    S01 - MVC01

---
+++ S01 - MVC01/SoluionMVC01/AplicacionMVC01/Controllers/BasicoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionMVC01.Controllers
+{
+    public class BasicoController : Controller
+    {
+        // GET: Basico
+        public ActionResult Index()
+        {
+            //Enviar datos desde ActionResult (Controller)
+            ViewBag.nombre = "Manuel Cabos";
+            ViewData["edad"] = 19;
+            return View();
+        }
+    }
+}
