a79534ab05795d39cf330ea5034a608f3d8da093
CabosManuel <cabos.apolaya@gmail.com>
2020-08-18 15:37:18 -0500

    S02 - MVC + Listar (CRUD)

---
+++ S02 - MVC + Listar (CRUD)/SolucionMVC_CRUD/AppMVC_CRUD/Controllers/AlumnoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AppMVC_CRUD.Models; //Acceso al Model
+
+namespace AppMVC_CRUD.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        static List<Alumno> alumnos = new List<Alumno>();
+
+        void setAlumnos()
+        {
+            Alumno alu1 = new Alumno()
+            {
+                codAlu = 101,
+                nomAlu = "Manuel Cabos",
+                pension = 460
+            };
+            Alumno alu2 = new Alumno()
+            {
+                codAlu = 102,
+                nomAlu = "Alejandro Apolaya",
+                pension = 460
+            };
+            alumnos.Add(alu1);
+            alumnos.Add(alu2);
+        }
+
+        // GET: Alumno
+        public ActionResult Listado()
+        {
+            setAlumnos();
+            return View(alumnos);
+        }
+    }
+}
---
+++ S02 - MVC + Listar (CRUD)/SolucionMVC_CRUD/AppMVC_CRUD/Models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppMVC_CRUD.Models
+{
+    public class Alumno
+    {
+        public int codAlu { get; set; }
+        public string nomAlu { get; set; }
+        public decimal pension { get; set; }
+    }
+}
