adfdc92db20a374f2ba96da674db25a1619850bb
CabosManuel <cabos.apolaya@gmail.com>
2020-08-18 16:03:10 -0500

    S03 - MVC + Crear Eliminar

---
+++ S03 - MVC + Crear Eliminar/SolucionMVC_CRUD/AppMVC_CRUD/Controllers/ALUMNOController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AppMVC_CRUD.Models; // acceder a los modelos
+
+namespace AppMVC_CRUD.Controllers
+{
+    public class ALUMNOController : Controller
+    {
+        static List<ALUMNO> Lista = new List<ALUMNO>();
+
+        void Poblar_Alumnos()
+        {
+            ALUMNO alu1 = new ALUMNO()
+            {
+                 codalu=101, nomalu="Victor Rojas", pension=420
+            };
+            ALUMNO alu2 = new ALUMNO()
+            {
+                codalu = 245, nomalu = "Susana Torres", pension = 400
+            };
+            //
+            Lista.Add(alu1);
+            Lista.Add(alu2);
+        }
+
+        // GET: ALUMNO
+        public ActionResult Listado()
+        {
+            // poblar los alumnos
+            // Poblar_Alumnos();
+            // enviar la lista de alumnos "Lista" a la vista
+            return View(Lista);
+        }
+
+        //////////////////////////
+        // GET
+        public ActionResult NuevoAlumno()
+        {
+            // mostrar una vista que nos permita ingresar los datos
+            // de un nuevo alumno
+            ALUMNO obj = new ALUMNO();
+            // enviar la instancia (objeto) de la clase ALUMNO
+            return View(obj);
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult NuevoAlumno(ALUMNO x)
+        {
+            int xcod = x.codalu;
+
+            // almacenar en la lista de alumnos
+            Lista.Add(x);
+
+            ViewData["mensaje1"] = "Alumno grabado correctamente";
+            ViewData.Add("mensaje2", "Alumno grabado OK");
+
+            ViewBag.MENSAJE = "Alumno: "+x.nomalu+" grabado exitosamente";
+
+            // ViewBag -> enviar datos desde el action a su vista
+            // ViewData -> enviar datos desde el action a su vista
+            //
+            // TempData -> enviar datos desde un action a otro (action)
+            // Session -> enviar datos desde un action a otro (action)
+
+            return View(x);
+        }
+
+
+        // GET: EliminarAlumno
+        public ActionResult EliminarAlumno(int codigo)
+        {
+            // encontrar los datos del Alumno a Eliminar
+            ALUMNO buscado = new ALUMNO();
+            //
+            foreach (var item in Lista)
+            {
+                if (item.codalu==codigo)
+                {
+                    buscado = item;
+                    break;
+                }
+            }
+
+            return View(buscado);
+        }
+
+
+        // POST: EliminarAlumno
+        [HttpPost]
+        public ActionResult EliminarAlumno(int codigo, ALUMNO obj)
+        {
+            ALUMNO encontrado = Lista.Find(z => z.codalu == codigo);
+
+            Lista.Remove(encontrado);
+
+            ViewBag.MENSAJE = "Alumno con codigo: "+codigo+" fue eliminado";
+
+            return View(obj);
+        }
+
+    }
+}
---
+++ S03 - MVC + Crear Eliminar/SolucionMVC_CRUD/AppMVC_CRUD/Models/ALUMNO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppMVC_CRUD.Models
+{
+    public class ALUMNO
+    {
+        // prop seguido de 2 veces tecla tab, para crear
+        // propiedades
+        public int codalu { get; set; }
+        public string nomalu { get; set; }
+        public decimal pension { get; set; }
+    }
+}
